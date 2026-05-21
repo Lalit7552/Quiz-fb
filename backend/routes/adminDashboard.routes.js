b81d2cf7167404a487909c3efa1be23a920ff78c
lucky <luckynihalchandani755@gmail.com>
2026-05-21 09:01:32 +0530

    project

--- backend/routes/adminDashboard.routes.js
+++ backend/routes/adminDashboard.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+const User = require("../models/User");
 const Category = require("../models/Category");
 const Question = require("../models/Question");
 const Result = require("../models/Result");
--- backend/routes/adminUser.routes.js
+++ backend/routes/adminUser.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+const User = require("../models/User");
 const Result = require("../models/Result");
 const auth = require("../middleware/auth");
 
